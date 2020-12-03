e0e9c82da1e6a48bea3f8f320329fc98892358f4
Patrick Moling <patrick.moling@marconirovereto.it>
2020-12-03 17:06:21 +0100

    mi sto documentando su come usare la giphy API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Sono un bellissimo script");
+let chiaveAPI = "HFqaiAe1aTenXLcQKxhGO8coltYQYbRW";
+let chiaveDiRicerca = "";
+let limiteElementiRestituiti = 0;
+let posizioneDiInizio = 0;
+let idProxy = "e826c9fc5c929e0d6c6d423841a282aa"
+
+function ricercaImmagine(){
+
+}
+
+ricercaImmagine();
