6fe9117f9b1a7ff80ae80e104c31257b546fe115
Patrick Moling <patrick.moling@marconirovereto.it>
2020-12-03 17:28:39 +0100

    non riesco ad arrivare a capo di niente

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 let chiaveAPI = "HFqaiAe1aTenXLcQKxhGO8coltYQYbRW";
-let chiaveDiRicerca = "";
-let limiteElementiRestituiti = 0;
-let posizioneDiInizio = 0;
-let idProxy = "e826c9fc5c929e0d6c6d423841a282aa"
 
 function ricercaImmagine(){
-
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=%${chiaveAPI}&limit=50')
+    .then(data => {
+        console.log(data);
+    });
 }
 
 ricercaImmagine();
